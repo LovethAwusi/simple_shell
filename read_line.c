228b65b96797c35d16a48d617f226faf97c6b2b2
LovethAwusi <awusieloho@gmail.com>
2023-10-06 11:42:51 -0700

    update

---
+++ read_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* read_line - reads d string input
+*
+* @i_eof: RV of the getline function
+* Return: string input
+*/
+char *read_line(int *i_eof)
+{
+char *input = NULL;
+size_t bufsize = 0;
+
+*i_eof = getline(&input, &bufsize, stdin);
+
+return (input);
+}
